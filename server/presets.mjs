8ccd796d55cb86eaba6c799f26945f1d44eb01cc
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-03-31 10:38:27 +0000

    fix: shorten the preview path

--- server/presets.mjs
+++ server/presets.mjs
@@ -243,10 +243,12 @@ export async function savePreset(name, preset) {
 export async function savePresetAssets(name, preset) {
   const { json, css } = preset;
   const basePath = join(CWD, 'presets', name);
+  const mjsFile = basePath + '.mjs';
+  const cssFile = basePath + '.css';
 
   await ensureFolder(dirname(basePath));
-  await writeFile(basePath + '.mjs', 'export default ' + json);
-  await writeFile(basePath + '.css', css);
+  await writeFile(mjsFile, 'export default ' + json);
+  await writeFile(cssFile, css);
   await mkdir(basePath, { recursive: true });
   await exec('npx', ['tailwind-config-viewer', 'export', '-c', mjsFile, basePath]);
 }
